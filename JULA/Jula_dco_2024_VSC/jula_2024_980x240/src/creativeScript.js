7859cfe413dfda03ae7af7e7ffbe8cc1472417b1
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-01-23 18:15:00 +0100

    980x240 Jula updates
    Added classes for price elements and price types

--- JULA/Jula_dco_2024_VSC/jula_2024_980x240/src/creativeScript.js
+++ JULA/Jula_dco_2024_VSC/jula_2024_980x240/src/creativeScript.js
@@ -26,36 +26,48 @@ onLemonpiReady(function () {
     // Advanced mapping of dynamic content
     // You can call the content directly once it's collected by lemonpi.subscribe method
     // Example content.[placeholder_name].value
-
-var product_collection = content.product_collection.value; 
-  var bgColor = content.bgColor.value;
-  $('#creative_container').css({
-    'background-color': bgColor,
-  })
-  var ctaCopy = content.ctaCopy.value;
-  var mainCopy = content.mainCopy.value; 
-  var productUrl = product_collection[0].productUrl.value;
-  $('#productUrl').html(productUrl)
-  var productName = product_collection[0].productName.value;
-  $('#productName').html(productName)
-        // Append price to product
-  var regularPrice = product_collection[0].regularPrice.value;
-  $('#regularPrice').html(regularPrice + ':-');
-      // Append image to product
-      var productImage =  product_collection[0].productImage.value;
-      $('#productImage').css({
-      backgroundImage: 'url("' + productImage + '")',
-      'background-size': 'contain',
-      'background-repeat': 'no-repeat',
-      });
     
-  //$('#regularPrice').html(regularPrice)
-  var logo = content.logo.value;
-  $('#logo').css({
-    'position': 'absolute',
-    'top': '20px',
+    //Background color of container
+    var bgColor = content.bgColor.value;
+    $('#creative_container').css({
+      'background-color': bgColor,
+    })
+
+    //Product collection from adset
+    var product_collection = content.product_collection.value; 
+    console.log(product_collection)
+    // Append name to product
+    var productName = product_collection[0].productName.value;
+    $('#productName').html(productName);
+    // Append price to product
+    var regularPrice = product_collection[0].regularPrice.value;
+    var productSaving = product_collection[0].productPriceSaving.value;
+    var productPriceType = 'julaclub';//product_collection[0].productPriceType.value;
+
+    $('#regularPrice').html(regularPrice + ',-');
+
+    if (productPriceType === 'regular') {
+      $('#regularPrice').addClass('regularPrice');
+    } else if (productPriceType === 'sale') {
+      $('#regularPrice').addClass('salePrice')
+    } else if (productPriceType === 'julaclub') {
+      $('#regularPrice').html('JulaClub <br><span style="font-size: 55px;">' + regularPrice + ',-</span>')
+      $('#regularPrice').addClass('clubPrice');
+    }
+    
+    console.log(regularPrice)
+    console.log(productSaving)
+    console.log(productPriceType)
+
+    // Append image to product
+    var productImage =  product_collection[0].productImage.value;
+    $('#productImage').css({
+    backgroundImage: 'url("' + productImage + '")',
+    'background-size': 'contain',
+    'background-repeat': 'no-repeat',
+    'background-position': 'center'
     });
-    });
+});
 
 // Get the div element by its id
  // Get the div element by its id
