b3306cf3232b5eefc9efb945ae67d97d2d9df60d
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-02-27 13:38:50 +0100

    Husqvarna DCO template
    Format 980x240 added and first commit

---
+++ Husqvarna/templates_2024/husqvarna_2024_980x240/src/creativeScript.js
@@ -0,0 +1,36 @@
+/**
+  * Template Name
+  * @Owner Developer Name 
+  * @Date
+*/
+
+function onLemonpiReady(cb) {
+  if (cb) {
+    var loadLemonpiTimerId = setInterval(function () {
+      if (window.lemonpi) {
+        clearInterval(loadLemonpiTimerId);
+        cb();
+      }
+    }, 0);
+  }
+}
+
+window.addEventListener('lemonpi.content/ready', event => {
+  const content = event.detail.content;
+  //Variable for local content
+  var local_content = content;
+
+  console.log(local_content)
+
+ //Append copy to frame 1
+  $("#copy_frame_1").html(local_content.frame_1_copy.value);
+  //Append copy to frame 2
+  $("#copy_frame_2").html(local_content.frame_2_copy.value);
+  //Append copy to cta
+  $("#cta").html(local_content.cta_copy.value);
+  //Append background image to image div    
+  $("#bg_image").css("background-image","url("+local_content.image_source.value+")");
+  //Append logo image to logo div    
+  $("#logo").css("background-image","url("+local_content.image_source.value+")");
+})
+  
