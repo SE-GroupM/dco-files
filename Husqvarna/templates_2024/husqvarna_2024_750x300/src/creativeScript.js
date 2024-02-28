907cb68caada3c65df24af5ccf8168682ea4ad78
Astrid Rönnborn <astrid.ronnborn@groupm.com>
2024-02-28 17:06:04 +0100

    15 hsq templates prepared
    15 hsq templates prepared with feed connections and html elements. Half of the templates contain adjusted CSS and JS settings

---
+++ Husqvarna/templates_2024/husqvarna_2024_750x300/src/creativeScript.js
@@ -0,0 +1,27 @@
+/**
+  * Template Name
+  * @Owner Developer Name 
+  * @Date
+*/
+
+function onLemonpiReady(cb) {
+  if (cb) {
+    var loadLemonpiTimerId = setInterval(function () {
+      if (window.lemonpi) {
+        clearInterval(loadLemonpiTimerId);
+        cb();
+      }
+    }, 0);
+  }
+}
+
+// Callback to retrieve the adset data
+onLemonpiReady(function () {
+  lemonpi.subscribe(function callback(content) {
+    // code here
+    // Advanced mapping of dynamic content
+    // You can call the content directly once it's collected by lemonpi.subscribe method
+    // Example content.[placeholder_name].value
+  });
+});
+  
