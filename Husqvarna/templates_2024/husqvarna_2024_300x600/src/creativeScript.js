9d39b63cfd54dc54e26d376c93e9039a0bcb76db
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-02-29 16:52:36 +0100

    Husqvarna updated formats
    980x240, 250x600, 300x600, 640x320, 300x300 updated with CSS and click

--- Husqvarna/templates_2024/husqvarna_2024_300x600/src/creativeScript.js
+++ Husqvarna/templates_2024/husqvarna_2024_300x600/src/creativeScript.js
@@ -3,7 +3,6 @@
   * @Owner Developer Name 
   * @Date
 */
-
 function onLemonpiReady(cb) {
   if (cb) {
     var loadLemonpiTimerId = setInterval(function () {
@@ -15,30 +14,30 @@ function onLemonpiReady(cb) {
   }
 }
 
-// Callback to retrieve the adset data
-onLemonpiReady(function () {
-  lemonpi.subscribe(function callback(content) {
-    // code here
-    // Advanced mapping of dynamic content
-    // You can call the content directly once it's collected by lemonpi.subscribe method
-    // Example content.[placeholder_name].value
-  });
-});
-
 window.addEventListener('lemonpi.content/ready', event => {
   const content = event.detail.content;
   //Variable for local content
   var local_content = content;
-  console.log(local_content)
+  //Append font size and line height on copy frames
+  $(".copy_frames").css({"font-size": local_content.copy_fontsize.value, "line-height": local_content.copy_fontsize.value});
  //Append copy to frame 1
   $("#copy_frame_1").html(local_content.frame_1_copy.value);
   //Append copy to frame 2
   $("#copy_frame_2").html(local_content.frame_2_copy.value);
   //Append copy to cta
   $("#cta").html(local_content.cta_copy.value);
+  //Append font size for cta
+  $("#cta").css("font-size", local_content.cta_fontsize.value);
   //Append background image to image div    
   $("#bg_image").css("background-image","url("+local_content.image_source.value+")");
   //Append logo image to logo div    
   $("#logo").css("background-image","url("+local_content.logo_source.value+")");
-})
+  // Get the div element by its id
+ var worldClickDiv = document.getElementById('creative_container');
   
+ // Add a click event listener to the div
+ worldClickDiv.addEventListener('click', function() {
+   // Opens the specified URL in a new window or tab
+   // window.open('');
+ });
+})
