bdbc04f7709af65789e971df16dafe1f78137eeb
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-02-29 17:39:25 +0100

    Animation added
    980x240, 300x600, 250x600, 320x320, 300x300, 640x320

--- Husqvarna/templates_2024/husqvarna_2024_300x600/src/creativeScript.js
+++ Husqvarna/templates_2024/husqvarna_2024_300x600/src/creativeScript.js
@@ -32,6 +32,40 @@ window.addEventListener('lemonpi.content/ready', event => {
   $("#bg_image").css("background-image","url("+local_content.image_source.value+")");
   //Append logo image to logo div    
   $("#logo").css("background-image","url("+local_content.logo_source.value+")");
+
+
+    /////////////////////
+  //// ANIMATIONS /////
+ ////////////////////
+
+// Animation loop timeline setup
+var t1 = new TimelineMax({
+  repeat: 5,  // Adjust repeat count accordingly
+  onComplete: function() {
+      t1.seek("#copy_frame_1");  // This will make the timeline stop at #copy_frame_1 animation
+  }
+  });
+
+  // label to identify the start of copy_frame_1 animation
+  t1.addLabel("#copy_frame_1")
+
+  // Start animation by setting initial lineHeight to 0
+  .set('#copy_frame_1, #copy_frame_2', { lineHeight: "7px" })
+
+  // fade in animation 
+  .to('#copy_frame_1', 0.3, { x:0 , opacity:1 , lineHeight: local_content.copy_fontsize.value, duration: 1.25, ease: Linear.ease })
+  // hold frame for 4 seconds
+  .to('#copy_frame_1', 2, { opacity:1, lineHeight: local_content.copy_fontsize.value, ease: Linear.easeNone , delay:0.5 })
+  // fade out animation over 1 second
+  .to('#copy_frame_1', 0.3, { opacity:0 , lineHeight: "9px", ease: Linear.easeNone })
+
+  // fade in animation - main copy over 1 second
+  .to('#copy_frame_2', 0.3, { x:0 , opacity:1 , lineHeight: local_content.copy_fontsize.value, ease: Linear.easeNone })
+  // hold frame for 4 seconds
+  .to('#copy_frame_2', 2, { opacity:1, lineHeight: local_content.copy_fontsize.value, ease: Linear.easeNone , delay:0.5 })
+  // fade out animation over 1 second
+  .to('#copy_frame_2', 0.3, { opacity:0 , lineHeight: "9px", ease: Linear.easeNone })
+  
   // Get the div element by its id
  var worldClickDiv = document.getElementById('creative_container');
   
