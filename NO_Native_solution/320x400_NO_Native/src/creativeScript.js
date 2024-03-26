8b374caed0489fc3cc44b678f7dcbc235bed11d4
Östling <joakim.ostling@groupm.com>
2024-03-26 14:55:09 +0100

    NO Native adjsutments
    Added functionality to change the font size, logo and logo position in the templates

--- NO_Native_solution/320x400_NO_Native/src/creativeScript.js
+++ NO_Native_solution/320x400_NO_Native/src/creativeScript.js
@@ -29,15 +29,23 @@ onLemonpiReady(function () {
     var mainCopy = local_content.mainText.value; // get mainCopy
     var imgCopy = local_content.imgText.value; // get imgCopy
     var imgSrc = local_content.imagePlaceholder.value; // get img src
-    var useFont = local_content.whichFont.value;
-    var publicist = local_content.publicist.value;
+    var publicist = local_content.publicist.value.toLowerCase();
     var panelTopValue = local_content.panelTopValue.value;
 
     // variables adjusting CSS top value for img, imgText and mainCopy
     var topvalueImgTxt = parseInt(content.topValue_imgTxt.value);
     var topValueMainTxt = parseInt(content.topValue_mainCopy.value);
     var topValueImg = parseInt(content.topValue_img.value);
+
+    // variables holding logo and adjusting position of it
+    var logo_src = local_content.logo_src.value;
+    var logo_top_placement = parseInt(local_content.logo_topplacement.value);
+    var logo_left_placement = parseInt(local_content.logo_leftplacement.value);
+    
     // css line heiught on mainCopy
+    // Added possibility to change fontsize on mainCopy
+    var mainCopy_fontSize = parseInt(local_content.mainText_fontSize.value);
+
     var lineHeightMainTxt = parseInt(content.lineHeight_mainCopy.value);
     // Append data to html in creative
     $('#mainCopy').html(mainCopy);
@@ -47,7 +55,7 @@ onLemonpiReady(function () {
       'top' : panelTopValue + 'px',
     });
 
-    if (publicist === "Amedia") {
+    if (publicist === "amedia") {
       $('#mainCopy').css({
         'font-family': 'Amedia_montserat_bold',
       });
@@ -64,7 +72,7 @@ onLemonpiReady(function () {
       $('#imgCopy').css({
         'font-family': 'TV2_regular',
       });
-    } else if (publicist === "Aller") {
+    } else if (publicist === "aller") {
       $('#mainCopy').css({
         'font-family': 'Arial Narrow-Bold',
       });
@@ -73,6 +81,13 @@ onLemonpiReady(function () {
       });
       $('#mainCopy').css({
         'font-size': '60px',
+      });
+    }
+
+    // adjust mainCopt fontsize
+    if (mainCopy_fontSize != '') {
+      $('#mainCopy').css({
+        'font-size': mainCopy_fontSize+'px',
       });
     }
 
@@ -121,5 +136,23 @@ onLemonpiReady(function () {
         'top': topValueImg + 'px', // adjust img position 
         });
     }
+
+    $('#logo_container').css({
+      backgroundImage: 'url("' + logo_src + '")',
+      'background-size': 'contain',
+      'background-repeat': 'no-repeat',
+      'background-position': 'left'
+      });
+
+      if (logo_top_placement === '' && logo_left_placement === '') {
+        // nothing changes
+      } else {
+        $('#logo_container').css({
+          'top': logo_top_placement + 'px', // adjust img position
+          'left': logo_left_placement + 'px', // left 
+          });
+      }
+
+    // end of code
   });
 });
--- NO_Native_solution/580x400_NO_Native/src/creativeScript.js
+++ NO_Native_solution/580x400_NO_Native/src/creativeScript.js
@@ -29,14 +29,22 @@ onLemonpiReady(function () {
     var mainCopy = local_content.mainText.value; // get mainCopy
     var imgCopy = local_content.imgText.value; // get imgCopy
     var imgSrc = local_content.imagePlaceholder.value; // get img src
-    var useFont = local_content.whichFont.value;
-    var publicist = local_content.publicist.value;
+    var publicist = local_content.publicist.value.toLowerCase();
     var panelTopValue = local_content.panelTopValue.value;
 
     // variables adjusting CSS top value for img, imgText and mainCopy
     var topvalueImgTxt = parseInt(content.topValue_imgTxt.value);
     var topValueMainTxt = parseInt(content.topValue_mainCopy.value);
     var topValueImg = parseInt(content.topValue_img.value);
+
+    // variables holding logo and adjusting position of it
+    var logo_src = local_content.logo_src.value;
+    var logo_top_placement = parseInt(local_content.logo_topplacement.value);
+    var logo_left_placement = parseInt(local_content.logo_leftplacement.value);
+
+    // Added possibility to change fontsize on mainCopy
+    var mainCopy_fontSize = parseInt(local_content.mainText_fontSize.value);
+
     // css line heiught on mainCopy
     var lineHeightMainTxt = parseInt(content.lineHeight_mainCopy.value);
     // Append data to html in creative
@@ -46,8 +54,8 @@ onLemonpiReady(function () {
     $('#panel_container').css({
       'top' : panelTopValue + 'px',
     });
-
-    if (publicist === "Amedia") {
+  
+    if (publicist.toLowerCase() === "amedia") {
       $('#mainCopy').css({
         'font-family': 'Amedia_montserat_bold',
       });
@@ -64,7 +72,7 @@ onLemonpiReady(function () {
       $('#imgCopy').css({
         'font-family': 'TV2_regular',
       });
-    } else if (publicist === "Aller") {
+    } else if (publicist === "aller") {
       $('#mainCopy').css({
         'font-family': 'Arial Narrow-Bold',
       });
@@ -76,6 +84,12 @@ onLemonpiReady(function () {
       });
     }
 
+    // adjust mainCopt fontsize
+    if (mainCopy_fontSize != '') {
+      $('#mainCopy').css({
+        'font-size': mainCopy_fontSize+'px',
+      });
+    }
     // Append static CSS
     if (topvalueImgTxt == ''){
       $('#imgCopy').css({
@@ -122,6 +136,24 @@ onLemonpiReady(function () {
           'top': topValueImg + 'px', // adjust img position 
           });
       }
+      
+      $('#logo_container').css({
+        backgroundImage: 'url("' + logo_src + '")',
+        'background-size': 'contain',
+        'background-repeat': 'no-repeat',
+        'background-position': 'left'
+        });
+
+        if (logo_top_placement === '' && logo_left_placement === '') {
+          // nothing changes
+        } else {
+          $('#logo_container').css({
+            'top': logo_top_placement + 'px', // adjust img position
+            'left': logo_left_placement + 'px', // left 
+            });
+        }
+
+  
 
     ///////////////////////////////////////////////////
     ////////////////// FUNCTIONS //////////////////////
