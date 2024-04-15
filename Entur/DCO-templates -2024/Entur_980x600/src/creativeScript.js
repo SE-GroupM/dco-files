27de5c0ab16c3d74fb62c94341ced17faaf8ef48
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-04-15 17:05:36 +0200

    980x600 template
    Animation code added and structure of animation in cities. Styling not done

--- Entur/DCO-templates -2024/Entur_980x600/src/creativeScript.js
+++ Entur/DCO-templates -2024/Entur_980x600/src/creativeScript.js
@@ -35,26 +35,30 @@ onLemonpiReady(function () {
     $("#logo_1").css("background-image","url("+local_content.logo_src_1.value+")");
     $("#logo_2").css("background-image","url("+local_content.logo_src_2.value+")");
     $("#logo_3").css("background-image","url("+local_content.logo_src_3.value+")");
-
     $("#logo_x").css("background-image","url(https://assets.lemonpi.io/a/k/e58bb9a7-86c0-4f6d-849f-2ed092ded49d/Assets/Assets/X_100px.png");
 
+    //Animation of content
+    var tl = new TimelineMax({repeat: -1, repeatDelay: 3});
+    TweenMax.set('#copyFrame_1, #copyFrame_2, #copyFrame_3, #logo_3', { x: 980 }) //Position of elements not used from start
+    tl.fromTo('#bgPanel', 0.3, {x:485,ease: Linear.ease},{x:0, ease: Linear.ease}, 1) //Background panel slides in
+      .fromTo('#bgImage', 0.3, {width:980, ease: Linear.ease},{width:495, ease: Linear.ease}, 1) //Background image changes width
+      .fromTo('#copyFrame_1', 0.3, {x:980, ease: Linear.ease},{x:0, ease: Linear.ease}, 1) //Copy frame 1 in
 
-    // //Animation of content
-    // var tl = new TimelineMax({repeat: -1, repeatDelay: 3});
-    // TweenMax.set('#copyFrame_1, #copyFrame_2, #copyFrame_3, #logo_3', { x: 980 }) //Position of elements not used from start
-    // tl.fromTo('#bgPanel', 0.3, {x:485},{x:0}, 1) 
-    //   .fromTo('#bgImage', 0.3, {width:980},{width:495}, 1) 
-    //   .fromTo('#copyFrame_1', 0.3, {x:980},{x:0}, 1) 
-    //   .to('#copyFrame_1', 0.3, {x:-980}, 3) 
-    //   .to('#bgPanel', 0.3, {x: -495}, 3) 
-    //   .fromTo('#copyFrame_2', 0.3, {x:980},{x:0}, 3) 
-    //   .fromTo('#logo_1', 0.3, {x:815},{x:0}, 3) 
-    //   .fromTo('#logo_2', 0.3, {x:30},{x:325}, 3) 
-    //   .fromTo('#logo_x', 0.3, {opacity:0},{opacity: 1, rotation: "+=360", repeat:1, ease: Linear.easeNone, transformOrigin:"50% 50%" }, 3)
-    //   .fromTo('#location_1', 0.3, {opacity:0, x:50},{opacity: 1, x:30}, 4) 
-    //   .to('#copyFrame_2', 0.3, {x:-980}, 7) 
-    //   .fromTo('#copyFrame_3', 0.3, {x:980},{x:0}, 7) 
-    //   .fromTo('#logo_3', 0.3, {x:980},{x:0}, 7)
+      .to('#copyFrame_1', 0.3, {x:-980, ease: Linear.ease}, 3) //Copy frame 1 out
+      .to('#bgPanel', 0.3, {x: -495, ease: Linear.ease}, 3) //Background panel covers creative
+      .fromTo('#copyFrame_2', 0.3, {x:980, ease: Linear.ease},{x:300, ease: Linear.ease}, 3) //Copy frame 2 in
+      .fromTo('#logo_1', 0.3, {x:815, ease: Linear.ease},{x:0, ease: Linear.ease}, 3) //Logo 1 to center
+      .fromTo('#logo_2', 0.3, {x:30, ease: Linear.ease},{x:325, ease: Linear.ease}, 3) //Logo 2 to center
+      .fromTo('#logo_x', 0.3, {opacity:0},{opacity: 1, rotation: "+=360", repeat:1, ease: Linear.ease, transformOrigin:"50% 50%" }, 3) //Logo x in
+
+      .fromTo('#location_1', 0.1, {opacity:1, y: 0, ease: Linear.ease},{opacity:0, y: 10, ease: Linear.ease}, 4) //Location 1 out
+      .fromTo('#location_2', 0.1, {opacity:0, y:-10, ease: Linear.ease},{opacity:1, y:0, ease: Linear.ease}, 4.3) //Location 2 in
+      .to('#location_2', 0.1, {opacity:0, y:10, ease: Linear.ease}, 5) //Location 2 out
+      .fromTo('#location_3', 0.1, {opacity:0, y:-10, ease: Linear.ease},{opacity:1, y: 0, ease: Linear.ease}, 5.3) //Location 3 in
+
+      .to('#copyFrame_2', 0.1, {x:-980, ease: Linear.ease}, 7) //Copy frame 2 out
+      .fromTo('#copyFrame_3', 0.3, {x:980, ease: Linear.ease},{x:0, ease: Linear.ease}, 7) //Copy frame 3 in
+      .fromTo('#logo_3', 0.3, {x:980},{x:0, ease: Linear.ease}, 7) //Logo 3 in
 
     //Append exit url to creative container
     document.getElementById('creative_container').onclick = () =>
