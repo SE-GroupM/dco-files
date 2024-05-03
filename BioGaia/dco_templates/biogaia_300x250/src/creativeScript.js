11092bd9c442ad646ed8c94f8ba3040b5904e6b3
Ann-Sofie Larsson <ann-sofie.larsson@groupm.com>
2024-05-03 14:53:01 +0200

    First format for BioGaia added
    300x250 added

--- BioGaia/dco_templates/biogaia_300x250/src/creativeScript.js
+++ BioGaia/dco_templates/biogaia_300x250/src/creativeScript.js
@@ -15,13 +15,49 @@ function onLemonpiReady(cb) {
   }
 }
 
-// Callback to retrieve the adset data
-onLemonpiReady(function () {
-  lemonpi.subscribe(function callback(content) {
-    // code here
-    // Advanced mapping of dynamic content
-    // You can call the content directly once it's collected by lemonpi.subscribe method
-    // Example content.[placeholder_name].value
-  });
+window.addEventListener('lemonpi.content/ready', event => {
+  const content = event.detail.content;
+  //Variable for local content
+  var local_content = content;
+  
+  //Variables for copy
+  var preCopy = local_content.copy_frame_1.value;
+  var store = local_content.department_store.value;
+    
+  //Append copy to div
+  $("#copy").html(preCopy + '<br>' + store);
+
+  //Variables for copy
+  var videoSrc = local_content.video_id.value;
+  var videoTracker = local_content.video_tracker.value;
+
+  //Video player 
+  var e = document.createElement('script');
+  e.src = 'https://video.seenthis.se/v2/player/74/player.js';
+  e.onload = function(){
+    var player = new SeenthisPlayer('.player', videoSrc, videoTracker, options); 
+  };
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(e, s);
+
+  //Options for video script
+  var options = {
+  loop: true,
+  autoplay: true,
+  muteButton: false,
+};
+
+
+  //Append exit url to creative container
+  document.getElementById('creative_container').onclick = () =>
+  window.dispatchEvent(
+      new CustomEvent('lemonpi.interaction/click', {
+      detail: {
+          placeholder: ['exit_url'],
+      }
+    })
+  );
+
 });
+  
   
