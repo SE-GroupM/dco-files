6dda014b2334dc7a7859c6f089b9df1de0f19242
Astrid Rönnborn <astrid.ronnborn@groupm.com>
2024-07-08 16:55:20 +0200

    Last few edits on default (regularPrice)
    Last few edits on default (regularPrice)

--- JULA/vib_propsecting_templates_with_products/jula_640x320_2024_vib_prospecting/src/creativeScript.js
+++ JULA/vib_propsecting_templates_with_products/jula_640x320_2024_vib_prospecting/src/creativeScript.js
@@ -1,9 +1,3 @@
-/**
-  * Template Name
-  * @Owner Developer Name 
-  * @Date
-*/
-
 function onLemonpiReady(cb) {
   if (cb) {
     var loadLemonpiTimerId = setInterval(function () {
@@ -16,49 +10,35 @@ function onLemonpiReady(cb) {
 }
 
 window.addEventListener('lemonpi.content/ready', event => {
-  const content = event.detail.content; // get content placeholders from Choreograph Adsets
+  const content = event.detail.content; 
   const source = event.detail.source;
 });
 
-// Callback to retrieve the adset data
 onLemonpiReady(function () {
   lemonpi.subscribe(function callback(content) {
-    
-    // Local content variable
     var local_content = content;
 
-     //////////////////////////////////////////////////////////////////////////////
-    /////////////////////////// VIDEO CONTAINER //////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
+    var videoSrc = local_content.videoSrc.value;
+    var videoTracker = local_content.videoTracker.value;
+    
+    var e = document.createElement('script');
+    e.src = 'https://video.seenthis.se/v2/player/74/player.js';
+    e.onload = function() {
+      var player = new SeenthisPlayer('.player', videoSrc, videoTracker, options);
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(e, s);
 
-     // Define the video source and tracker variables
-     var videoSrc = local_content.videoSrc.value;
-     var videoTracker = local_content.videoTracker.value;
- 
-     //Video player
-     var e = document.createElement('script');
-     e.src = 'https://video.seenthis.se/v2/player/74/player.js';
-     e.onload = function(){
-     var player = new SeenthisPlayer('.player', videoSrc, videoTracker, options);
-     };
-     var s = document.getElementsByTagName('script')[0];
-     s.parentNode.insertBefore(e, s);
- 
-       //Options for video script
-     var options = {
-         loop: true,
-         loopCount: 3,
-         autoplay: true,
-         muteButton: true,
-     };
+    var options = {
+      loop: true,
+      loopCount: 3,
+      autoplay: true,
+      muteButton: true,
+    };
 
-    // Background color of container
     var bgColor = local_content.bgColor.value;
-    $('#creative_container').css({
-      'background-color': bgColor,
-    });
+    $('#creative_container').css('background-color', bgColor);
 
-    // CTA placeholders from CC
     var ctaText = local_content.ctaText.value;
     $('#ctaText').html(ctaText);
     $('#ctaText').css({
@@ -66,127 +46,86 @@ onLemonpiReady(function () {
       'color': local_content.ctaTextColor.value,
     });
 
-    // Background color of product box
     var productBgColor = local_content.productBgColor.value;
-    $('#productBox').css({
-      'background-color': productBgColor,
-    });
+    $('#productBox').css('background-color', productBgColor);
 
-    //Text color of product name
     var productNameColor = local_content.productNameColor.value;
-    $('#productName').css({
-      'color': productNameColor,
-    });
-    
-    // Controlls styling and content of campaign text/header text
-    var campaignText = local_content.campaignText.value;
-    $('#campaignText').html(campaignText)
-    $('#campaignText').css({
-       'color': productNameColor,
-    })
+    $('#productName').css('color', productNameColor);
 
-    // Product collection from adset
+    var campaignText = local_content.campaignText.value;
+    $('#campaignText').html(campaignText).css('color', productNameColor);
+
     var local_product_collection = local_content.product_collection.value;
 
-    // Function to update the product display
     function updateProductDisplay(index) {
       var product = local_product_collection[index];
 
-      // Append name to product
-      //$('#productName').html(product.productName.value);
+      function numberWithSpaces(x) {
+        var parts = parseFloat(x).toFixed(2).split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        return parts.join(".");
+      }
 
-       // Function to format numbers with spaces
-     function numberWithSpaces(x) {
-      var parts = parseFloat(x).toFixed(2).split(".");
-      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-      return parts.join(".");
-     }
- 
- // Append price to product
- var regularPrice = product.regularPrice.value;
- var productPriceType = product.productPriceType.value;
- 
- // Format the regular price with spaces
- var formattedRegularPrice = numberWithSpaces(regularPrice);
- 
- // Split formatted price to find decimals
- var tempNormal = formattedRegularPrice.split(".");
+      var regularPrice = product.regularPrice.value;
+      var productPriceType = product.productPriceType.value;
+
+      var formattedRegularPrice = numberWithSpaces(regularPrice);
+
+      var tempNormal = formattedRegularPrice.split(".");
       
- // Append sup class on decimals
- if (parseInt(tempNormal[1]) > 0) {
-   // Ex. 88.88
-   $('#regularPrice').html(tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + ' </span>');
- } else {
-   // Ex. 88,-
-   $('#regularPrice').html(tempNormal[0] + '<span style="letter-spacing: -6px; padding-right: 8px;">.-</span>');
- }
- 
- // Format the product price type with spaces 
- var formattedProductPriceType = numberWithSpaces(productPriceType);
- 
- //formating productPriceType
- $('#productPriceType').html(formattedProductPriceType);
- 
-       // Check product price type and append css
-       if (productPriceType === 'regular') {
-         // Text color of product price
-         var productPriceColor = local_content.productPriceColor.value;
-         $('#regularPrice').css({
-           'color': productPriceColor,
-         });
-       } else if (productPriceType === 'sale') {
-         // If product price type is sale
-         if (tempNormal[1] > 0) {
-           // Ex. 88.88
-           $('#regularPrice').html(tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + ' </span>');
-         } else {
-           // Ex. 88,-
-           $('#regularPrice').html(tempNormal[0] + '<span style="letter-spacing: -6px; padding-right: 8px;">.-</span>');
-         }
-         $('#regularPrice').addClass('salePrice');
-       } else if (productPriceType === 'julaclub') {
-         // If product price type is Jula club
-         if (tempNormal[1] > 0) {
-           // Ex. 88.88
-           $('#regularPrice').html('JulaClub <br><span style="font-size: 52px; line-height: 58px;">' + tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + '</span></span>');
-         } else {
-           // Ex. 88,-
-           $('#regularPrice').html('JulaClub <br><span style="font-size: 52px; line-height: 58px;">' + tempNormal[0] + '<span style="letter-spacing: -7px; padding-right: 8px;">.-</span>');
-         }
-         $('#regularPrice').addClass('clubPrice');
-       }
+      if (parseInt(tempNormal[1]) > 0) {
+        $('#regularPrice').html(tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + ' </span>');
+      } else {
+        $('#regularPrice').html(tempNormal[0] + '<span style="letter-spacing: -6px; padding-right: 8px;">.-</span>');
+      }
 
-      // Saving element ex. '60.-'
+      var formattedProductPriceType = numberWithSpaces(productPriceType);
+
+      $('#productPriceType').html(formattedProductPriceType);
+      
+      if (productPriceType === 'regular') {
+        var productPriceColor = local_content.productPriceColor.value;
+        $('#regularPrice').css('color', productPriceColor);
+      } else if (productPriceType === 'sale') {
+        if (tempNormal[1] > 0) {
+          $('#regularPrice').html(tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + ' </span>');
+        } else {
+          $('#regularPrice').html(tempNormal[0] + '<span style="letter-spacing: -6px; padding-right: 8px;">.-</span>');
+        }
+        $('#regularPrice').addClass('salePrice');
+      } else if (productPriceType === 'julaclub') {
+        if (tempNormal[1] > 0) {
+          $('#regularPrice').html('JulaClub <br><span style="font-size: 52px; line-height: 58px;">' + tempNormal[0] + '<span class="priceSup">' + tempNormal[1] + '</span></span>');
+        } else {
+          $('#regularPrice').html('JulaClub <br><span style="font-size: 52px; line-height: 58px;">' + tempNormal[0] + '<span style="letter-spacing: -7px; padding-right: 8px;">.-</span>');
+        }
+        $('#regularPrice').addClass('clubPrice');
+      }
+
       var productSaving = product.productPriceSaving.value;
-      // Slice '.-' to style it according to guidelines
       productSaving = productSaving.replace(".-", "");
-      // Check if product saving is > 0 and append saleElement class
       if (productSaving !== "0") {
         $('#priceElement').html('Spara ' + productSaving + '<span style="letter-spacing: -1px; padding-right: 2px;">.-</span>');
         $('#priceElement').addClass('saleElement');
       }
 
-      // Check if price type is 'Tokbilligt' or kalasproduct (update since week 11 on site) and append heroElement class and salePrice class
       if (productPriceType.toLowerCase().includes('kalasprodukt')) {
         $('#regularPrice').addClass('salePrice');
         $('#priceElement').html(productPriceType);
         $('#priceElement').addClass('heroElement');
       }
 
-      // Append image to product
       var productImage = product.productImage.value;
       $('#productImage').css({
-        backgroundImage: 'url("' + productImage + '")',
+        'background-image': 'url("' + productImage + '")',
         'background-size': 'contain',
         'background-repeat': 'no-repeat',
         'background-position': 'center'
       });
 
-      // Append lowest price last 30 days
       var priceInfo = product.productLatestPrice.value;
       $('#priceInfo').html(priceInfo);
 
-      // Update click URL
       $('#worldClick').off('click').on('click', function () {
         window.dispatchEvent(
           new CustomEvent('lemonpi.interaction/click', {
@@ -198,7 +137,6 @@ onLemonpiReady(function () {
       });
     }
 
-    // Animation to loop through products
     var widthOnBanner = 250;
     var currentProductIndex = 0;
     var main_timeline = new TimelineMax({ repeat: -1, delay: 0.2, onRepeat: function () {
@@ -207,15 +145,41 @@ onLemonpiReady(function () {
     } });
 
     main_timeline
-    .fromTo('#productBox', 0.2, { opacity: 0, x: widthOnBanner }, { opacity: 1, x: 0, ease: "power2.out" }, 0.1) // Fast fade in
-    .fromTo('#productInfo', 0.5, { opacity: 0 }, { opacity: 1, ease: "power2.out" }, 0.1) // Fade in product info
-    .to('#productBox', 0.5, { x: 0, ease: "power1.out" }, "+=0.2") // Slow down and stay for a bit
-    .to('#productBox', 0.3, { x: widthOnBanner, opacity: 0 }, "+=3.5") // Stay for 3.5 seconds and fade out
-    .to('#productInfo', 0.5, { opacity: 0, ease: "power1.out" }, "-=0.3") // Fade out product info
-    .set('#productBox', { x: widthOnBanner }); // Reset to start position for seamless looping
+    .fromTo('#productBox', 0.2, { opacity: 0, x: widthOnBanner }, { opacity: 1, x: 0, ease: "power2.out" }, 0.1)
+    .fromTo('#productInfo', 0.5, { opacity: 0 }, { opacity: 1, ease: "power2.out" }, 0.1)
+    .to('#productBox', 0.5, { x: 0, ease: "power1.out" }, "+=0.2")
+    .to('#productBox', 0.3, { x: widthOnBanner, opacity: 0 }, "+=3.5")
+    .to('#productInfo', 0.5, { opacity: 0, ease: "power1.out" }, "-=0.3")
+    .set('#productBox', { x: widthOnBanner });
 
-    // Initialize display with the first product
     updateProductDisplay(currentProductIndex);
-
   });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Function to detect if the user is on Windows and using Edge/Chrome/similar
+  function isWindowsAndModernBrowser() {
+    const userAgent = navigator.userAgent;
+
+    const isWindows = userAgent.indexOf('Windows') > -1;
+    const isEdge = userAgent.indexOf('Edg') > -1;
+    const isChrome = userAgent.indexOf('Chrome') > -1 && !isEdge;
+
+    return isWindows && (isEdge || isChrome);
+  }
+
+  // If the condition is met, inject additional CSS
+  if (isWindowsAndModernBrowser()) {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      .saleElement {
+        top: 135px !important;
+      }
+      .heroElement {
+        top: 135px !important;
+      }
+    `;
+    document.head.appendChild(style);
+  }
 });
