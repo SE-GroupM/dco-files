b09eb0689f40cde07fde1146b9650ccd42629bb7
Östling <joakim.ostling@groupm.com>
2025-06-17 14:23:46 +0200

    Updated the truncate limit on some formats for subText

--- Mercuri Urval/300x600_2025/src/creativeScript.js
+++ Mercuri Urval/300x600_2025/src/creativeScript.js
@@ -136,7 +136,7 @@ onLemonpiReady(function () {
      if (jobDesTrunc) {
        truncateProductText('#subText', jobDesTrunc);
      } else {
-       truncateProductText('#subText', 130);
+       truncateProductText('#subText', 150);
      }
      $("#locationText").html(jobLocation);
     
